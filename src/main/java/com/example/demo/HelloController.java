dc22549242aec571e91d805ccee429b9fb77562b
Takuya <moldigai@yahoo.co.jp>
2026-06-12 11:42:18 +0900

    formからControllerに変数渡すことができた

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,16 +3,24 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(Model model) {
+    @GetMapping("/")
+    public String index() {
+        System.out.println("htmlを呼ぶ");
+        return "hello";
+    }
+
+    @PostMapping("/hello")
+    @ResponseBody
+    public String hello(@RequestParam String message) {
         System.out.println("hello呼ばれた");
 
-        model.addAttribute("message", "こんにちは");
-        model.addAttribute("name", "ボブ");
-        return "hello";
+        return "受け取った値：" + message;
     }
 }
