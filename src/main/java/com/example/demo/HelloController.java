84c8cab1db6f1c01c6ba3a3fdddb8c7d6804faea
Takuya <moldigai@yahoo.co.jp>
2026-06-12 11:13:36 +0900

    Controllerから変数を注入しhtmlで表示できるようになった

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model) {
         System.out.println("hello呼ばれた");
+
+        model.addAttribute("message", "こんにちは");
+        model.addAttribute("name", "ボブ");
         return "hello";
     }
 }
