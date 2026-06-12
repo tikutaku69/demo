ce033bdcd725b9cbfcf36673bd309f823f73a73f
Takuya <moldigai@yahoo.co.jp>
2026-06-12 10:57:31 +0900

    thymeleaf依存追加とhtmlファイルを追加し、helloで呼び出せた

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         System.out.println("hello呼ばれた");
-        return "Hello Spring!";
+        return "hello";
     }
 }
