a7b47ceffdacf43daac0afcaa66c8c918bf4b672
Takuya <moldigai@yahoo.co.jp>
2026-06-19 11:44:23 +0900

    ModelAttributeにより、引数削減、責務分離、デバッグ切り分けのしやすさを得た

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,8 +3,8 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -16,11 +16,11 @@ public class HelloController {
     }
 
     @PostMapping("/hello")
-    public String hello(@RequestParam String message, @RequestParam String message2, Model model) {
+    public String hello(@ModelAttribute HelloForm form, Model model) {
         System.out.println("hello呼ばれた");
 
-        model.addAttribute("message", message);
-        model.addAttribute("message2", message2);
+        model.addAttribute("message", form.getMessage());
+        model.addAttribute("message2", form.getMessage2());
         return "result";
     }
 }
---
+++ src/main/java/com/example/demo/HelloForm.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class HelloForm {
+
+    private String message;
+    private String message2;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage2() {
+        return message2;
+    }
+
+    public void setMessage2(String message2) {
+        this.message2 = message2;
+    }
+}
