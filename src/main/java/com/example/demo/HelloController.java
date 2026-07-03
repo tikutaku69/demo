77f79ae482c214ebb3d65e487af272b9d7c6ac79
tikutaku69 <132460261+tikutaku69@users.noreply.github.com>
2026-07-03 10:34:21 +0900

    HTML追加 (#1)

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
+    @GetMapping("/")
+    public String index() {
+        System.out.println("htmlを呼ぶ");
+        return "hello";
+    }
+
+    @PostMapping("/hello")
+    public String hello(@ModelAttribute HelloForm form, Model model) {
         System.out.println("hello呼ばれた");
-        return "Hello Spring!";
+
+        model.addAttribute("message", form.getMessage());
+        model.addAttribute("message2", form.getMessage2());
+        return "result";
     }
 }
