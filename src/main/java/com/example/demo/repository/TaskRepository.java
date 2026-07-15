a1f5f65061df7b28c75c668a1559f43401dcfd33
Takuya <moldigai@yahoo.co.jp>
2026-07-15 19:58:33 +0900

    保存先をArrayListからDBであるh2に変えた

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -1,21 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.dto.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class TaskRepository {
-
-    private final List<Task> tasks = new ArrayList<>();
-
-    public void save(Task task) {
-        tasks.add(task);
-    }
-
-    public List<Task> findAll() {
-        return tasks;
-    }
-}
+public interface TaskRepository extends JpaRepository<Task, Long>{}
