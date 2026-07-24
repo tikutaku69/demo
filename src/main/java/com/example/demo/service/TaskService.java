34fb5389acf287a8c918f589795dccc52544f1d8
tikutaku69 <132460261+tikutaku69@users.noreply.github.com>
2026-07-24 17:14:20 +0900

    各タスクの更新機能を追加 (#6)

--- src/main/java/com/example/demo/service/TaskService.java
+++ src/main/java/com/example/demo/service/TaskService.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -20,7 +21,18 @@ public class TaskService {
         taskRepository.save(task);
     }
 
+    public void updateTask(Long id, String title) {
+        Optional<Task> task = taskRepository.findById(id);
+        Task updateTask = task.get();  //ここでOptionalからTask型を取り出す
+        updateTask.setTitle(title);  //Task型だからsetTitleを呼べる
+        taskRepository.save(updateTask);
+    }
+
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
     }
 }
